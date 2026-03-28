072f81c5c8ed11a75c74156f4439cceb855fb943
Nikolay <nbaskoff@gmail.com>
2026-03-28 22:30:57 +0700

    Contract: rename container.php to contract.php in configuration loading

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../Core/Path.php';
 Path::initFromPublic(__DIR__);
 require_once Path::root('Core/Autoloader.php');
 Autoloader::register();
-Contract::loadConfig(Path::root('config/container.php'));
+Contract::loadConfig(Path::root('config/contract.php'));
 $router = new Router();
 $router->loadRoutes(Path::root('config/routes.php'));
 $router->dispatch();
