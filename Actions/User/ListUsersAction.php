39130f76b71e232829b39a9513d7aab716b0da97
Nikolay <nbaskoff@gmail.com>
2026-03-29 23:56:42 +0700

    Enhance ListUsersAction to include user roles and update template to display roles with admin badge

--- Actions/User/ListUsersAction.php
+++ Actions/User/ListUsersAction.php
@@ -2,27 +2,41 @@
 
 namespace Actions\User;
 
+use Core\Contract;
+use Contracts\DatabaseContract;
 use Repositories\UserRepository;
 
 /**
- * Экшен получения списка пользователей
+ * Экшен получения списка пользователей с их ролями
  */
 class ListUsersAction
 {
-    public function __construct(
-        private UserRepository $userRepo
-    ) {}
+    private UserRepository $userRepo;
+
+    public function __construct()
+    {
+        // Получаем базу через контракт и создаем репозиторий вручную
+        $db = Contract::make(DatabaseContract::class);
+        $this->userRepo = new UserRepository($db);
+    }
 
     /**
-     * Позволяет вызывать класс как функцию: $action()
+     * Подготавливает данные для шаблона
      *
-     * @return array Данные для передачи в View
+     * @return array
      */
     public function __invoke(): array
     {
+        $users = $this->userRepo->all();
+
+        // Подгружаем роли для каждого пользователя
+        foreach ($users as $user) {
+            $user->roles = $this->userRepo->getUserRoles($user->id);
+        }
+
         return [
-            'users' => $this->userRepo->all(),
-            'title' => 'Список пользователей'
+            'users' => $users,
+            'title' => 'Список пользователей и их роли'
         ];
     }
 }
--- assets/templates/user/index.php
+++ assets/templates/user/index.php
@@ -1,22 +1,39 @@
 <h1><?= $title ?></h1>
 
-<table border="1">
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>Имя</th>
-        <th>Email</th>
-        <th>Дата регистрации</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($users as $user): ?>
-        <tr>
-            <td><?= $user->id ?></td>
-            <td><?= $user->name ?></td>
-            <td><?= $user->email ?></td>
-            <td><?= $user->created_at ?></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
+<table border="1" cellpadding="10" cellspacing="0">
+	<thead>
+	<tr>
+		<th>ID</th>
+		<th>Имя</th>
+		<th>Email</th>
+		<th>Роли</th>
+		<th>Дата регистрации</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($users as $user): ?>
+		<tr>
+			<td><?= $user->id ?></td>
+			<td>
+				<?= $user->name ?>
+				<?php if ($user->hasRole('admin')): ?>
+					<span style="color: red;">[ADM]</span>
+				<?php endif; ?>
+			</td>
+			<td><?= $user->email ?></td>
+			<td>
+				<?php if (!empty($user->roles)): ?>
+					<?php
+					// Извлекаем только названия (name) из массива ролей
+					$roleNames = array_column($user->roles, 'name');
+					echo implode(', ', $roleNames);
+					?>
+				<?php else: ?>
+					<i style="color: gray;">нет ролей</i>
+				<?php endif; ?>
+			</td>
+			<td><?= $user->getFormattedDate() ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
 </table>
