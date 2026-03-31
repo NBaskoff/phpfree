9357bf3e41f63301ad8a3e841fa13a83cc8a6932
Nikolay <nbaskoff@gmail.com>
2026-04-01 04:57:55 +0700

    Update `webpack.mix.js`: enable versioning, disable notifications, and refine build comments

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,22 @@
 const mix = require('laravel-mix');
 
+// папка для манифеста и скомпилированных файлов
 mix.setPublicPath('public');
 
-// --- СБОРКА ДЛЯ САЙТА (Корень /public) ---
+// --- сборка для сайта (корень /public) ---
 mix.js('assets/js/app.js', 'js/site.js');
 mix.sass('assets/scss/app.scss', 'css/site.css');
 mix.copyDirectory('assets/img', 'public/img');
 mix.copyDirectory('assets/fonts', 'public/fonts');
 
-// --- СБОРКА ДЛЯ АДМИНКИ (Папка /public/admin) ---
+// --- сборка для админки (папка /public/admin) ---
 mix.js('assets/admin/js/app.js', 'admin/js/admin.js');
 mix.sass('assets/admin/scss/app.scss', 'admin/css/admin.css');
 mix.copyDirectory('assets/admin/img', 'public/admin/img');
 mix.copyDirectory('assets/admin/fonts', 'public/admin/fonts');
 
+// включение версионности для сброса кэша
 mix.version();
+
+// отключение системных уведомлений
 mix.disableNotifications();
